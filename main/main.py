516f59ecd337ad80d24a2fcdf9c0291361bff8f7
Davi Barreto <davivbarreto@gmail.com>
2017-09-29 03:29:52 +0300

    Fixed bug that accepted any last input as correct. Fixed a few redundancies in the code, and changed comments a bit. Also game now gets faster as the sequence gets longer (up to a certain point).

--- main/main.py
+++ main/main.py
@@ -9,12 +9,39 @@ from functools import partial
 import threading
 
 
+# GUI file must be "<same name of this class>.kv" all lowercase
+# GUI builder class (at the end of file) must be "<same name replacing "Layout"
+# with "App">"(App) case sensitive
 class SimonBoxLayout(BoxLayout):
+    """ Game logic goes inside this class."""
 
-    # reset all class variables
-    def set_class_variables(self):
+    # binded to newgame button
+    def setup(self, a, b, c, d):
+        ''' Receives colored buttons objects.'''
 
-        # kivy button objects of the colored squares
+        # init/reset variables for new game
+        self.set_game_variables()
+        # sleep to allow older threads to die
+        sleep(0.1)
+
+        # setup class variables
+        self.objcs = [a, b, c, d]
+        self.rand_list = [randint(0, 3) for i in range(self.starting_size - 1)]
+        self.game_on = True
+
+        # setup game screen
+        display_streak = 'Current streak: ' + str(self.current_streak)
+        display_record = 'Current record: ' + str(self.longest_streak)
+        self.streak.text = display_streak
+        self.record.text = display_record
+
+        # start game loop
+        threading.Thread(target=self.newgame).start()
+
+    # init/reset all game variables
+    def set_game_variables(self):
+
+        # kivy button objects for the colored squares
         self.objcs = []
 
         # random new sequence that player will try replicate
@@ -27,13 +54,10 @@ class SimonBoxLayout(BoxLayout):
         self.current_streak = 0
 
         # current longest registered sequence replicate
-        self.longest_streak = 0
+        self.longest_streak = self.load_record()
 
         # starting lenght of the sequence
         self.starting_size = 1
-
-        # current game tracker for how many numbers in sequence
-        self.move_counter = 0
 
         # game is still ongoing, used to continue looping game
         self.game_on = False
@@ -50,28 +74,6 @@ class SimonBoxLayout(BoxLayout):
         # kill_thread_flag is used to kill python loops after game closes
         self.kill_thread_flag = threading.Event()
 
-    # binded to newgame button
-    def setup(self, a, b, c, d):
-
-        self.set_class_variables()
-
-        # setup class variables
-        self.objcs = [a, b, c, d]
-        self.current_streak = 0
-        self.longest_streak = self.load_record()
-        self.move_counter = self.starting_size - 1
-        self.rand_list = [randint(0, 3) for i in range(self.move_counter)]
-        self.game_on = True
-
-        # setup game screen
-        display_streak = 'Current streak: ' + str(self.current_streak)
-        display_record = 'Current record: ' + str(self.longest_streak)
-        self.streak.text = display_streak
-        self.record.text = display_record
-
-        # start game loop
-        threading.Thread(target=self.newgame).start()
-
     # game loop
     def newgame(self):
         while self.game_on:
@@ -79,16 +81,13 @@ class SimonBoxLayout(BoxLayout):
             if self.kill_thread_flag.is_set():
                 # if yes kill loop
                 return
-            self.pattern()
-            self.intakepattern()
+            self.output_pattern()
+            self.intake_pattern()
             self.update_current()
         self.announce_gameover()
 
     # schedule the sequence
-    def pattern(self):
-
-        # increment lenght of the sequence
-        self.move_counter += 1
+    def output_pattern(self):
 
         # add new value to sequence
         self.rand_list.append(randint(0, 3))
@@ -96,53 +95,39 @@ class SimonBoxLayout(BoxLayout):
         # lock player input while sequence being shown
         self.change_turn()
 
-        # in order to not move too fast:
-        buff = self.speed / 4
-        sleep(3 * buff)
+        # time buffer between events in order to not move too fast for humans:
+        buff = self.update_self_speed()
+        sleep(7 * buff)
 
-        # list of functions to dim and turn back on
-        # each button in sequence
+        # list of functions to blink (dim/turnon) each button in sequence
         dim_list = []
         turnon_list = []
-        for i in self.rand_list[:self.move_counter]:
+        for i in self.rand_list:
             obj = self.objcs[i]
             partial_func1 = partial(self.showpattern_dim, obj)
             partial_func2 = partial(self.showpattern_high, obj)
             dim_list.append(partial_func1)
             turnon_list.append(partial_func2)
 
-        # scheduling the time of execution of each function, in order
-        # to create the sequence flow
+        # scheduling the time of execution of each function,
+        # in order to create the sequence flow.
+        # the buffer is used to create the blink effect
         for i in range(len(dim_list)):
-            Clock.schedule_once(dim_list[i], i * self.speed * 1.1)
-            Clock.schedule_once(turnon_list[i], i * self.speed * 1.1 + 1)
+            # schedule turning button off
+            Clock.schedule_once(dim_list[i], i * (self.speed) + buff)
+            # schedule turning button back on
+            Clock.schedule_once(turnon_list[i], (i + 1) * (self.speed))
 
-        # unlock player input block, after entire sequence was shown
-        Clock.schedule_once(self.change_turn, i * self.speed * 1.1 + 1 + buff)
+        # allow player's input after entire sequence was shown
+        Clock.schedule_once(self.change_turn, (i + 1) * (self.speed))
 
-    # dim button (recieves *args scheduling passes extra arg "dt")
-    def showpattern_dim(self, obj, *args):
-        obj.background_color[-1] = 0.2
+    # get player's input
+    def intake_pattern(self, *args):
 
-    # brighten button (recieves *args scheduling passes extra arg "dt")
-    def showpattern_high(self, obj, *args):
-        obj.background_color[-1] = 1
-
-    # update if it's player turn to play or not
-    def change_turn(self, *args):
-        self.players_turn = not self.players_turn
-        if self.players_turn:
-            self.turn.text = "YOUR TURN!"
-        else:
-            self.turn.text = ("REPEAT THIS SEQUENCE")
-
-    # update if it's player turn to play or not
-    def intakepattern(self, *args):
-
-        # reset the players input from previous round, to get new one
+        # reset the players input from previous round
         self.player_moves = []
 
-        # wait for players turn to be True
+        # wait for players turn
         while not self.players_turn:
 
             # check if program was closed
@@ -157,10 +142,10 @@ class SimonBoxLayout(BoxLayout):
         # This loop will check and make sure every click matches sequence.
         # Will exit when player number of inputs equals the lenght of the
         # sequence.
-        while len(self.player_moves) < self.move_counter:
+        while True:
 
-            # check if program was closed
-            if self.kill_thread_flag.is_set():
+            # check if program was closed or new game was pressed
+            if self.kill_thread_flag.is_set() or not self.game_on:
                 # if yes kill loop
                 return
 
@@ -172,18 +157,27 @@ class SimonBoxLayout(BoxLayout):
                     self.game_on = False
                     return
 
+            # return when player has reproduced the entire sequence
+            if len(self.player_moves) == len(self.rand_list):
+
+                # first confirm last input was indeed correct
+                if self.player_moves[-1] != self.rand_list[-1]:
+                    # if different, declare game over
+                    self.game_on = False
+                return
+
             # wait a little before continuing loop
             sleep(0.1)
 
     # update screen after every turn
     def update_current(self):
 
-        # if you failed to complete this last sequence lenght
+        # define current streak
         if not self.game_on:
-            self.move_counter -= 1
-
-        # update current streak
-        self.current_streak = self.move_counter
+            self.current_streak = (len(self.rand_list) - 1 if
+                                   len(self.rand_list) > 0 else 0)
+        else:
+            self.current_streak = len(self.rand_list)
 
         # if your streak is bigger than your record, update record
         if self.current_streak > self.longest_streak:
@@ -196,24 +190,13 @@ class SimonBoxLayout(BoxLayout):
         self.streak.text = streak
         self.record.text = record
 
-    # load record from storage file
-    def load_record(self):
-        try:
-            with open("kivy.dll") as f:
-                data = f.readline()
-                return int(data)
-        except FileNotFoundError:
-            with open("kivy.dll", mode="w") as f:
-                f.write("0")
-                return 0
-
     # if game is over, announce it
     def announce_gameover(self):
 
         # if there was a new record, update file, and congratulate
         if self.new_record_flag:
             with open("kivy.dll", mode="w") as f:
-                f.write(str(self.current_streak))
+                f.write(str(hex(self.current_streak)))
 
             announce = "GAMEOVER\nCongratulations!\nYour new record is "
             announce += str(self.current_streak) + " repetitions."
@@ -223,6 +206,33 @@ class SimonBoxLayout(BoxLayout):
             announce += str(self.longest_streak) + " repetitions."
             self.turn.text = (announce)
 
+    # dim button (recieves *args because scheduling passes extra arg "dt")
+    def showpattern_dim(self, obj, *args):
+        obj.background_color[-1] = 0.2
+
+    # brighten button
+    def showpattern_high(self, obj, *args):
+        obj.background_color[-1] = 1
+
+    # update if it's player turn to play or not
+    def change_turn(self, *args):
+        self.players_turn = not self.players_turn
+        if self.players_turn:
+            self.turn.text = "YOUR TURN!"
+        else:
+            self.turn.text = ("REPEAT THIS SEQUENCE")
+
+    # load record from storage file
+    def load_record(self):
+        try:
+            with open("kivy.dll") as f:
+                data = f.readline()
+                return int(data, 16)
+        except FileNotFoundError:
+            with open("kivy.dll", mode="w") as f:
+                f.write("0")
+                return 0
+
     # bound to colored buttons
     def click_append(self, color_number):
         # if its player turn, append to list else don't.
@@ -230,9 +240,21 @@ class SimonBoxLayout(BoxLayout):
             if self.players_turn and self.game_on:
                 self.player_moves.append(color_number)
         except AttributeError:
-            self.set_class_variables()
+            self.set_game_variables()
+
+    # increment speed with every move
+    def update_self_speed(self):
+        ''' Updates the speed of the game in order to go faster as sequences get longer
+            Outputs the appropriate time buffer between blinks and other events
+        '''
+
+        self.speed = round(self.speed - self.speed / 10, 2)
+        self.speed = 0.4 if self.speed < 0.4 else self.speed
+
+        return round(self.speed / 10, 2)
 
 
+# build and stop GUI
 class SimonGameApp(App):
 
     def on_stop(self):
